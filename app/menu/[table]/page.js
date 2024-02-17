234ec042c86f3f822485433a1201c904df0320d3
Vedant Shah <vedantshah2001@gmail.com>
2024-02-17 18:10:14 +0530

    cart ui, customizations

--- app/menu/[table]/page.js
+++ app/menu/[table]/page.js
@@ -2,23 +2,30 @@
 import React, { useEffect, useState } from "react";
 
 import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
 
 import MenuItem from "@/components/MenuItem";
 import SkeletonMenuItem from "@/components/SkeletonMenuItem";
-
+import { Label } from "@/components/ui/label";
+import { TbFaceIdError } from "react-icons/tb";
 function TableOrder({ params }) {
   //constants
   const Cryptr = require("cryptr");
   const cryptr = new Cryptr("myTotallySecretKey");
   let decryptedTableNo;
+  const [searchValue, setSearchValue] = useState("");
 
   //states
   const [tableNo, setTableNo] = useState();
   const [allDishes, setAllDishes] = useState([]);
+  const [vegOnly, setVegOnly] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   //use-effects
   useEffect(() => {
     setTableNo(cryptr.decrypt(params.table));
     localStorage.setItem("tableNo", decryptedTableNo);
+    setLoading(true);
     getAllDishes();
   }, []);
 
@@ -27,7 +34,38 @@ function TableOrder({ params }) {
     const response = await fetch("/api/getalldishes");
     const data = await response.json();
     setAllDishes(data.data);
+    setLoading(false);
   };
+
+  const filteredDishes = allDishes.filter((val) => {
+    if (vegOnly) {
+      if (searchValue === "" && val.isVeg) return val;
+      else if (
+        val.name.toLowerCase().includes(searchValue.toLowerCase()) &&
+        val.isVeg
+      )
+        return val;
+    } else {
+      if (searchValue === "") return val;
+      else if (val.name.toLowerCase().includes(searchValue.toLowerCase()))
+        return val;
+    }
+  });
+
+  const dishItems = filteredDishes.map((dish) => {
+    return <MenuItem dish={dish} key={dish.id} />;
+  });
+
+  const displayMessage = (
+    <div className="flex flex-col items-center justify-center h-96">
+      <TbFaceIdError className="text-9xl" />
+
+      <h1 className="my-3 text-xl text-center">
+        Uh Oh! <br /> Looks like there are No items to display. <br /> Kindly
+        vary search parameters!
+      </h1>
+    </div>
+  );
 
   return (
     <>
@@ -35,12 +73,22 @@ function TableOrder({ params }) {
         <h1 className="text-4xl font-extrabold tracking-tight mb-7 scroll-m-20 lg:text-5xl">
           Hey there!
         </h1>
-        <Input className="mb-4" placeholder="Search" />
-        {allDishes.length > 0 ? (
-          allDishes.map((dish) => {
-            return <MenuItem dish={dish} />;
-          })
-        ) : (
+        <Input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="mb-4"
+          placeholder="Search"
+        />
+        <div className="flex items-center my-4">
+          <Switch
+            checked={vegOnly}
+            onCheckedChange={setVegOnly}
+            className="data-[state=checked]:bg-green-500 me-3 "
+          />
+          <Label>Veg Only</Label>
+        </div>
+        {dishItems.length > 0 ? dishItems : !loading && displayMessage}
+        {loading && (
           <>
             <SkeletonMenuItem />
             <SkeletonMenuItem />
--- components/Navbar.js
+++ components/Navbar.js
@@ -13,18 +13,47 @@ import {
   SheetTrigger,
   SheetFooter,
 } from "@/components/ui/sheet";
+import { Textarea } from "@/components/ui/textarea";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Badge } from "./ui/badge";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
 
 function Navbar() {
-  const { isEmpty, totalItems, items, updateItemQuantity, removeItem } =
-    useCart();
+  const {
+    cartTotal,
+    totalItems,
+    items,
+    updateItemQuantity,
+    removeItem,
+    setItems,
+  } = useCart();
   const [cartCount, setCartCount] = useState();
+  const [cartItemsCopy, setCartItemsCopy] = useState(items);
 
   useEffect(() => {
     setCartCount(totalItems);
   }, [items]);
 
+  useEffect(() => {
+    const courseOrder = { starter: 0, main: 1, deserts: 2 };
+    let temp = items;
+    temp.sort((a, b) => courseOrder[a.course] - courseOrder[b.course]);
+    setItems(temp);
+  }, []);
+
+  const getCurrentCustomizations = (dish) => {
+    let cus = [];
+    Object.keys(dish.chosenCustomization).forEach((cust) => {
+      cus = cus.concat(dish.chosenCustomization[cust]);
+    });
+    let str = cus.toString().slice(0, 17) + "...";
+
+    return str;
+  };
   return (
-    <div className="w-100 h-[10vh] flex items-center sticky p-4 justify-between">
+    <div className="w-100 h-[10vh] flex items-center sticky p-4 justify-between top-0 z-[49] bg-[#090909]">
       <Button variant="secondary">
         <FileTextIcon className="w-4 h-4 mr-2" />
         Bill
@@ -39,12 +68,111 @@ function Navbar() {
             </Button>
           </div>
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader className="h-[10vh]">
-            <SheetTitle>Your Order</SheetTitle>
+        <SheetContent
+          onOpenAutoFocus={(e) => e.preventDefault()}
+          className="flex flex-col">
+          <SheetHeader className="h-[10vh]" style={{ flex: "0 0 auto" }}>
+            <SheetTitle className="text-3xl">Your Order</SheetTitle>
           </SheetHeader>
-          <div className="h-[75vh]"></div>
-          <SheetFooter className="h-[10vh]">Hello</SheetFooter>
+          <ScrollArea className="h-[65vh]" style={{ flex: "1 1 auto" }}>
+            {items.map((dish) => {
+              return (
+                <>
+                  <div className="flex justify-between mb-10">
+                    <div className="w-[50%] flex">
+                      <div className="h-[100%] flex items-start justify-center">
+                        {dish.isVeg ? (
+                          <img
+                            src="https://img.icons8.com/color/480/vegetarian-food-symbol.png"
+                            style={{ width: "15px", height: "15px" }}
+                            className="inline-flex me-3"
+                            alt=""
+                          />
+                        ) : (
+                          <Image
+                            src="/non-veg.png"
+                            style={{ width: "15px", height: "15px" }}
+                            className="inline-flex me-3"
+                            width={15}
+                            height={15}
+                            alt=""
+                          />
+                        )}
+                      </div>
+                      <div>
+                        <h1 className="inline-flex mb-2">{dish.name}</h1>
+                        <h1 className="text-xs text-gray-400">
+                          {dish.availableCustomizations &&
+                            getCurrentCustomizations(dish)}
+                        </h1>
+                      </div>
+                    </div>
+                    <div className="flex items-center">
+                      <Badge
+                        onClick={() => {
+                          updateItemQuantity(
+                            items.filter((item) => item._id === dish._id)[0].id,
+                            items.filter((item) => item._id === dish._id)[0]
+                              .quantity - 1
+                          );
+                        }}
+                        style={{ borderRadius: "5px 0 0 5px" }}>
+                        -
+                      </Badge>
+                      <Badge variant="secondary" style={{ borderRadius: "0" }}>
+                        {
+                          items.filter((item) => item._id === dish._id)[0]
+                            ?.quantity
+                        }
+                      </Badge>
+                      <Badge
+                        onClick={() => {
+                          updateItemQuantity(
+                            items.filter((item) => item._id === dish._id)[0].id,
+                            items.filter((item) => item._id === dish._id)[0]
+                              .quantity + 1
+                          );
+                        }}
+                        style={{ borderRadius: "0 5px  5px 0" }}>
+                        +
+                      </Badge>
+                    </div>
+                    <h3 className="flex items-center">
+                      ₹ {dish.price * dish.quantity}
+                    </h3>
+                  </div>
+                </>
+              );
+            })}
+          </ScrollArea>
+          <div
+            className="flex flex-col justify-end "
+            style={{ flex: "0 0 auto" }}>
+            <input type="text" autoFocus="true" className="hidden" />
+            <Textarea
+              className="my-3"
+              placeholder="Any Special Instructions?"
+            />
+            {/* <Separator className="my-3" /> */}
+            <div className="flex flex-col">
+              <h1 className="mb-2 font-bold">Bill Details</h1>
+              <div className="flex justify-between ">
+                <h1 className="text-lg ">Items Total</h1>
+                <h1 className="text-lg ">₹ {cartTotal}</h1>
+              </div>
+              <div className="flex justify-between mb-5">
+                <h1 className="text-lg ">GST</h1>
+                <h1 className="text-lg ">₹ {0.05 * cartTotal}</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mb-5">
+              <h1 className="text-xl font-extrabold">TO PAY</h1>
+              <h1 className="text-xl font-extrabold">
+                ₹ {cartTotal + 0.05 * cartTotal}
+              </h1>
+            </div>
+            <Button>Order</Button>
+          </div>
         </SheetContent>
       </Sheet>
     </div>
